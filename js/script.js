173f1679ffe82f026db0203db5f4d368dd2e2817
Jonathan <jonrcao@gmail.com>
2016-05-23 17:51:42 +0800

    修复导航条的按钮显示问题，为适配1920显示器做准备

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 /* Todo
- * 通过导航条的ul列表实现原网页的间隙不跳转。
+ * 1.鼠标滑入导航菜单再滑出，banner 不会停止轮换。
  * 
+ * 2.把 title 和download 元素放置到与其相对应的 banner li 标签里，
+ *   直接控制 banner li 的动画即可。压缩 jQuery 和 CSS 的代码量。
+ * 
+ * 3.banner 按钮和标题1920分辨率下显示的问题，看是否能通过2方法解决。
+ * 
+ * 关于在1920分辨率下 banner 图片显示问题：
+ * 是通过 jQuery 响应浏览器窗口尺寸，
+ * 动态修改 banner-warp 元素的 height 值（固定的比例计算出结果），
+ * 并切换显示适合当前屏幕浏览的 banner 图片，
+ * 目前先以当前浏览器尺寸为主，写完大部分布局后再做1920分辨率的适配。
  */
 
 //页面加载执行开始
@@ -8,18 +18,20 @@ $(document).ready(function() {
   var index = 0;
   var maximg = 2;
   
-  //导航条鼠标滑过效果
+  //鼠标滑过导航条效果
   $(".nav-ul > li").hover(function (){
-    if ($(this).attr("class") != "current"){
-      $(this).css("background-color", "#09b4f9")
-      .siblings("li")
+      if ($(this).children().attr("class") != "current"){
+        $(this).children("a").css("background-color", "#09b4f9");
+        $(this).siblings().children("a")
+        .css("background-color", "transparent");
+      }
+  }, function (){
+    if($(this).children().attr("class") != "current"){
+      $(".nav-ul > li .current")
+      .css("background-color", "#09b4f9");
+      $(this).children("a")
       .css("background-color", "transparent");
     }
-  }, function (){
-    $(".nav-ul .current")
-    .css("background-color", "#09b4f9")
-    .siblings("li")
-    .css("background-color", "transparent");
   });
   
   //鼠标悬停在banner点上的事件
@@ -69,17 +81,17 @@ $(document).ready(function() {
 function ShowjQueryFlash(i) {
   $(".banner > li").eq(i)
     .animate({opacity: 0},1000)
-    .css({"z-index": "0"})
+    .css({"z-index": "-2"})
     .siblings()
     .animate({opacity: 1},1000)
-    .css({"z-index": "1"});
+    .css({"z-index": "-1"});
     
-$(".btn-txt > h2").eq(i)
+  $(".btn-txt > h2").eq(i)
     .animate({opacity: 0},1000)
-    .css({"z-index": "0"})
+    .css({"z-index": "-2"})
     .siblings()
     .animate({opacity: 1},1000)
-    .css({"z-index": "1"});
+    .css({"z-index": "-1"});
     
   $(".nav-dot > span").eq(i-1)
     .addClass("nav-dot-on")
